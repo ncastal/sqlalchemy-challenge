1fb02f1e185578ad0b06a280c29c47cf896797e2
ncastal <nick.cast89@gmail.com>
2020-02-07 10:06:37 -0500

    updated app.py to give json of all data for each date between start and end dates, updated climate-starter

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -72,23 +72,79 @@ def temperature():
        temp.append(temp_dict)
    return jsonify(temp)
 
-#returns json of temp max, min, and average from a start date to end of data
+#returns json of temp max, min, and average from evry date between start date to end of data
 @app.route('/api/v1.0/<start>')
 def start_avg(start):
+    #create datetime object from <start>
+    start_date_str=start.split('-')
+    start_year_int=int(start_date_str[0])
+    start_month_int=int(start_date_str[1])
+    start_day_int=int(start_date_str[2])
+    start_date=dt.date(start_year_int,start_month_int,start_day_int)
     session=Session(engine)
-    result=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start).all()
-    temps=list(np.ravel(result))
-    return jsonify(temps)
+    #create datetime object from last date in data
+    last_date=session.query(func.strftime("%Y-%m-%d",Measurement.date)).order_by(Measurement.date.desc()).first()
+    end_date=last_date[0].split('-')
+    end_year_int=int(end_date[0])
+    end_month_int=int(end_date[1])
+    end_day_int=int(end_date[2])
+    end_date=dt.date(end_year_int,end_month_int,end_day_int)
+    #create list of date between start and last date
+    date_list=[]
+    date=start_date
+    while date!=end_date:
+        date_list.append(date)
+        date=date+dt.timedelta(days=1)
+    date_list.append(end_date)
+    #formatted list for query
+    formated_date_list=[]
+    for date in date_list:
+        date=date.strftime('%Y-%m-%d')
+        formated_date_list.append(date)    
+    temp_list=[]
+    #loop through formatted date list to query for average, max, and min temps each date
+    for date in formated_date_list:
+        result= session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date==date).all()
+        temps=result[0]
+        temp_list.append(temps)
+    return jsonify(temp_list)
 
-#returns json of temp max, min, and average from a start date to an end date
+#returns json of temp max, min, and average of every date between a start date to an end date
 @app.route('/api/v1.0/<start>/<end>')
 def start_end_avg(start,end):
+    #start date datetime object
+    start_date_str=start.split('-')
+    start_year_int=int(start_date_str[0])
+    start_month_int=int(start_date_str[1])
+    start_day_int=int(start_date_str[2])
+    start_date=dt.date(start_year_int,start_month_int,start_day_int)
+    #end date datetime object
+    end_date_str=end.split('-')
+    end_year_int=int(end_date_str[0])
+    end_month_int=int(end_date_str[1])
+    end_day_int=int(end_date_str[2])
+    end_date=dt.date(end_year_int,end_month_int,end_day_int)
     session=Session(engine)
-    result= session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start).filter(Measurement.date<=end).all()
-    temps=list(np.ravel(result))
-    return jsonify(temps)
+    #list of dates between start date and end date
+    date_list=[]
+    date=start_date
+    while date!=end_date:
+        date_list.append(date)
+        date=date+dt.timedelta(days=1)
+    date_list.append(end_date)
+    formated_date_list=[]
+    for date in date_list:
+        date=date.strftime('%Y-%m-%d')
+        formated_date_list.append(date)    
+    temp_list=[]
+    #loop through date list to query for avg, max, and min temps between start and end date
+    for date in formated_date_list:
+        result= session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date==date).all()
+        temps=result[0]
+        temp_list.append(temps)
+    return jsonify(temp_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
